1ce00c850e7eccd8e70d902225736dced21ee126
Jim Crist <jcrist@continuum.io>
2017-08-04 14:33:31 -0500

    Use versioneer

--- dask_gdf/__init__.py
+++ dask_gdf/__init__.py
@@ -1,1 +1,5 @@
 from .core import from_pygdf, from_dask_dataframe
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+import versioneer
 
-version = '0.0.1'
 packages = ['dask_gdf']
 
 setup(name='dask_gdf',
-      version=version,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='A Partitioned GPU DataFrame',
       url='https://github.com/gpuopenanalytics/dask_gdf',
       packages=packages,
