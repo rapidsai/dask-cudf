963fdc633ad5f10c39dab822872c4001c6f2b8a5
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-08-24 16:07:37 -0500

    Add test for query

--- dask_gdf/tests/test_core.py
+++ dask_gdf/tests/test_core.py
@@ -7,6 +7,8 @@ import dask_gdf as dgd
 
 
 def test_from_pygdf():
+    np.random.seed(0)
+
     df = pd.DataFrame({'x': np.random.randint(0, 5, size=10000),
                        'y': np.random.normal(size=10000)})
 
@@ -19,3 +21,21 @@ def test_from_pygdf():
     # Test conversion to dask.dataframe
     ddf = ingested.to_dask_dataframe()
     assert_frame_equal(ddf.compute(), df)
+
+
+def test_query():
+    np.random.seed(0)
+
+    df = pd.DataFrame({'x': np.random.randint(0, 5, size=10),
+                       'y': np.random.normal(size=10)})
+    gdf = gd.DataFrame.from_pandas(df)
+    expr = 'x > 2'
+
+    assert_frame_equal(gdf.query(expr).to_pandas(), df.query(expr))
+
+    queried = (dgd.from_pygdf(gdf, npartitions=2).query(expr))
+
+    got = queried.compute().to_pandas()
+    expect = gdf.query(expr).to_pandas()
+
+    assert_frame_equal(got, expect)
