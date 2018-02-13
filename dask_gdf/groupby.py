641cb85820cf7395f38e7bd8e6cbaad8f0c7b787
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-02-13 14:15:17 -0600

    Fix groupby reduce

--- dask_gdf/groupby.py
+++ dask_gdf/groupby.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from dask.delayed import delayed
 from dask import compute, persist
+import pygdf
 
 from .core import from_delayed
 
@@ -33,25 +34,29 @@ class Groupby(object):
         # First, do groupby on the first key by sorting on the first key.
         # This will sort & shuffle the partitions.
         firstkey = self._by[0]
-        df = self._df.sort_values_binned(firstkey)
+        df = self._df.sort_values(firstkey)
         groups = df.to_delayed()
         # Second, do groupby internally for each partition.
         @delayed
         def _groupby(df, by):
             grouped = df.groupby(by=by)
-            ovdata = _extract_data_to_check_group_overlap(grouped, by)
-            return grouped, ovdata
+            return grouped
 
-        grouped = [_groupby(g, self._by) for g in groups]
-        # Persist the groupby operation to avoid duplicating the work
-        grouped = persist(*grouped)
         # Get the groupby objects
-        outgroups = list(map(delayed(operator.itemgetter(0)), grouped))
-        _check_group_non_overlap_assumption(grouped)
-        return outgroups
+        grouped = [_groupby(g, self._by) for g in groups]
+        return grouped
 
-    def _aggregation(self, reducer):
-        parts = [delayed(reducer)(g) for g in self._grouped]
+    def _aggregation(self, chunk, combine, split_every=8):
+        by = self._by
+
+        def cat_and_group(*dfs):
+            return pygdf.concat(dfs).reset_index().groupby(by)
+
+        parts = [delayed(chunk)(g) for g in self._grouped]
+        while len(parts) > 1:
+            chunked = _chunk_every(parts, split_every)
+            parts = [delayed(cat_and_group)(*c) for c in chunked]
+            parts = [delayed(combine)(g) for g in parts]
         return from_delayed(parts).reset_index()
 
     def apply(self, function):
@@ -79,38 +84,68 @@ class Groupby(object):
     # Aggregation APIs
 
     def count(self):
-        return self._aggregation(lambda g: g.count())
+        return self._aggregation(lambda g: g.count(),
+                                 lambda g: g.sum())
+
+    def sum(self):
+        return self._aggregation(lambda g: g.count(),
+                                 lambda g: g.sum())
 
     def mean(self):
-        return self._aggregation(lambda g: g.mean())
+        valcols = set(self._df.columns) - set(self._by)
+
+        def combine(df):
+            outdf = df[:1].loc[:, list(self._by)]
+            for k in valcols:
+                sumk = '{}_sum'.format(k)
+                countk = '{}_count'.format(k)
+                outdf[k] = df[sumk].sum() / df[countk].sum()
+            return outdf
+
+        return self._aggregation(lambda g: g.agg(['sum', 'count']),
+                                 lambda g: g.apply(combine),
+                                 split_every=None)
 
     def max(self):
-        return self._aggregation(lambda g: g.max())
+        return self._aggregation(lambda g: g.max(),
+                                 lambda g: g.max())
 
     def min(self):
-        return self._aggregation(lambda g: g.min())
+        return self._aggregation(lambda g: g.min(),
+                                 lambda g: g.min())
 
-    def std(self):
-        return self._aggregation(lambda g: g.std())
+    def std(self, ddof=1):
+        valcols = set(self._df.columns) - set(self._by)
+
+        def combine(df):
+            outdf = df[:1].loc[:, list(self._by)]
+            for k in valcols:
+                sosk = '{}_sum_of_squares'.format(k)
+                sumk = '{}_sum'.format(k)
+                countk = '{}_count'.format(k)
+                the_sos = df[sosk].sum()
+                the_sum = df[sumk].sum()
+                the_count = df[countk].sum()
+
+                div = the_count - ddof
+                mu = the_sum / the_count
+                var = the_sos / div - (mu ** 2) * the_count / div
+
+                outdf[k] = np.sqrt(var)
+
+            return outdf
+
+        return self._aggregation(
+            lambda g: g.agg(['sum_of_squares', 'sum', 'count']),
+            lambda g: g.apply(combine),
+            split_every=None)
 
 
-def _extract_data_to_check_group_overlap(grouped, by):
-    """
-    See _check_group_non_overlap_assumption()
-    """
-    interim, _ = grouped.as_df()
-    limits = interim.loc[:, by].take(np.asarray([0, len(interim) - 1]))
-    pdlimits = limits.to_pandas()
-    first = pdlimits.iloc[0].to_dict()
-    last = pdlimits.iloc[1].to_dict()
-    return grouped, (first, last)
-
-
-def _check_group_non_overlap_assumption(grouped):
-    # Check that the groups do not overlap.
-    # NOTE: the implementation of the aggregation functions is
-    #       assuming non-overlapping groups.
-    limits = compute(map(delayed(operator.itemgetter(1)), grouped))
-    for (_, last), (cur, _) in zip(limits, limits[1:]):
-        if last == cur:
-            raise NotImplementedError("unexpected overlay of group")
+def _chunk_every(seq, every):
+    group = []
+    for x in seq:
+        group.append(x)
+        if every is not None and len(group) == every:
+            yield group
+            group = []
+    yield group
