6b2be1fc94054afacdf41edcaa123593d16f06c3
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-02-21 14:59:23 -0600

    Better tests

--- dask_gdf/tests/test_join.py
+++ dask_gdf/tests/test_join.py
@@ -1,36 +1,54 @@
-import numpy as np
-import pandas as pd
-from pandas.util.testing import assert_frame_equal
-
 import pytest
+import numpy as np
 
 import pygdf as gd
 import dask_gdf as dgd
-import dask.dataframe as dd
-
+from functools import partial
 
 param_nrows = [5, 10, 100, 400]
 
 @pytest.mark.parametrize('left_nrows', param_nrows)
 @pytest.mark.parametrize('right_nrows', param_nrows)
-def test_join_inner(left_nrows, right_nrows):
+@pytest.mark.parametrize('left_nkeys', [4, 5])
+@pytest.mark.parametrize('right_nkeys', [4, 5])
+def test_join_inner(left_nrows, right_nrows, left_nkeys, right_nkeys):
     chunksize = 50
 
     np.random.seed(0)
 
-    left = gd.DataFrame({'x': np.random.randint(0, 5, size=left_nrows),
+    # PyGDF
+    left = gd.DataFrame({'x': np.random.randint(0, left_nkeys,
+                                                size=left_nrows),
                          'a': np.arange(left_nrows)}.items())
-    right = gd.DataFrame({'x': np.random.randint(0, 5, size=right_nrows),
-                          'b': np.arange(right_nrows)}.items())
+    right = gd.DataFrame({'x': np.random.randint(0, right_nkeys,
+                                                 size=right_nrows),
+                          'b': 1000 * np.arange(right_nrows)}.items())
 
     expect = left.set_index('x').join(right.set_index('x'), how='inner', sort=True)
     expect = expect.to_pandas()
 
+    # Dask GDf
     left = dgd.from_pygdf(left, chunksize=chunksize)
     right = dgd.from_pygdf(right, chunksize=chunksize)
 
     joined = left.set_index('x').join(right.set_index('x'), how='inner')
     got = joined.compute().to_pandas()
 
+    # Check index
     np.testing.assert_array_equal(expect.index.values,
                                   got.index.values)
+
+    # Check rows in each groups
+    expect_rows = {}
+    got_rows = {}
+
+    def gather(df, grows):
+        grows[df['index'].values[0]] = (set(df.a), set(df.b))
+
+    expect.reset_index().groupby('index')\
+        .apply(partial(gather, grows=expect_rows))
+
+    expect.reset_index().groupby('index')\
+        .apply(partial(gather, grows=got_rows))
+
+    assert got_rows == expect_rows
