2473b5297efc7c52ed302bdcd14f25be503a973f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-09-16 17:48:07 -0400

    Add more tests

--- dask_gdf/tests/test_accessor.py
+++ dask_gdf/tests/test_accessor.py
@@ -17,16 +17,6 @@ def data1():
 fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
 
 
-@pytest.mark.parametrize('data', [data()])
-def test_series(data):
-    pd_data = pd.Series(data.copy())
-    gdf_data = Series(pd_data)
-    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
-    np.testing.assert_equal(
-        np.array(pd_data), 
-        np.array(dask_gdf_data.compute()),
-        )
-
 @pytest.mark.parametrize('data', [data1()])
 @pytest.mark.xfail(raises=AttributeError)
 def test_datetime_accessor_initialization(data):
@@ -34,3 +24,27 @@ def test_datetime_accessor_initialization(data):
     gdf_data = Series(pd_data)
     dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
     dask_gdf_data.dt 
+
+
+@pytest.mark.parametrize('data', [data()])
+def test_series(data):
+    pd_data = pd.Series(data.copy())
+    gdf_data = Series(pd_data)
+    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
+
+    np.testing.assert_equal(
+        np.array(pd_data), 
+        np.array(dask_gdf_data.compute()),
+        )
+
+
+@pytest.mark.parametrize('data', [data()])
+@pytest.mark.parametrize('field', fields)
+def test_dt_series(data, field):
+    pd_data = pd.Series(data.copy())
+    gdf_data = Series(pd_data)
+    dask_gdf_data = dgd.from_pygdf(gdf_data, npartitions=5)
+    base = getattr(pd_data.dt, field)
+    test = getattr(dask_gdf_data.dt, field).compute()\
+                                           .to_pandas().astype('int64')
+    assert_series_equal(base, test)
