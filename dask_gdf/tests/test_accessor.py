7a753cd4613355eab570ee2c78e61ddcc0a9a8b4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-09-16 17:15:27 -0400

    add datetime accessor test

--- dask_gdf/tests/test_accessor.py
+++ dask_gdf/tests/test_accessor.py
@@ -10,6 +10,10 @@ def data():
     return pd.date_range('20010101', '20020215', freq='400h')
 
 
+def data1():
+    return np.random.randn(100)
+
+
 fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
 
 
@@ -22,3 +26,11 @@ def test_series(data):
         np.array(pd_data), 
         np.array(dask_gdf_data.compute()),
         )
+
+@pytest.mark.parametrize('data': [data1()])
+def test_datetime_accessor_initialization(data):
+    pd_data = pd.Series(data.copy())
+    gdf_data = Series(pd_data)
+    dask_gdf_data = dgd.from_dask_dataframe(gdf_data, npartitions=5)
+    with pytest.raises(AttributeError):
+        dask_gdf_data.dt  
