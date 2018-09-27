be7c1c883804b7a9d8adcb3ecd79b94b324aa460
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-09-27 02:52:07 -0600

    remove uncommented code

--- dask_gdf/accessor.py
+++ dask_gdf/accessor.py
@@ -92,11 +92,10 @@ class CachedAccessor(object):
     Parameters
     ----------
     name : str
-        The namespace this will be accessed under, e.g. ``df.foo``
+        The namespace this will be accessed under, e.g. ``df.timestamp.dt``
     accessor : cls
         The class with the extension methods. The class' __init__ method
-        should expect one of a ``Series``, ``DataFrame`` or ``Index`` as
-        the single argument ``data``
+        should expect a ``Series`` as the single argument ``data``
     """
 
     def __init__(self, name, accessor):
@@ -108,11 +107,6 @@ class CachedAccessor(object):
             # we're accessing the attribute of the class, i.e., Dataset.geo
             return self._accessor
         accessor_obj = self._accessor(obj)
-        # Replace the property with the accessor object. Inspired by:
-        # http://www.pydanny.com/cached-property.html
-        # We need to use object.__setattr__ because we overwrite __setattr__ on
-        # NDFrame
-        # object.__setattr__(obj, self._name, accessor_obj)
         return accessor_obj
 
 
