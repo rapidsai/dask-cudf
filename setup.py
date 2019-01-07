99a669dd93c4577f7d746e22c189bf9761ed3a4a
Matthew Rocklin <mrocklin@gmail.com>
2019-01-07 14:02:54 -0800

    Add dask_cudf.io to setup.py packages (#60)

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from os.path import exists
 from setuptools import setup
 import versioneer
 
-packages = ['dask_cudf',
-            'dask_cudf.tests']
+packages = ["dask_cudf", "dask_cudf.io"]
 
-setup(name='dask_cudf',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='A Partitioned GPU DataFrame',
-      license="Apache Software License 2.0",
-      url='https://github.com/rapidsai/dask_cudf',
-      long_description=(open('README.rst').read() if exists('README.rst')
-                        else ''),
-      packages=packages,
-      zip_safe=False)
+packages = packages + [p + ".tests" for p in packages]
+
+setup(
+    name="dask_cudf",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="A Partitioned GPU DataFrame",
+    license="Apache Software License 2.0",
+    url="https://github.com/rapidsai/dask_cudf",
+    long_description=(open("README.rst").read() if exists("README.rst") else ""),
+    packages=packages,
+    zip_safe=False,
+)
