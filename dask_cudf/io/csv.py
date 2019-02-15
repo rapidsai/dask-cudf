0c76bb4b6906123917f6301a6471b0e916b3619d
Benjamin Zaitlen <quasiben@users.noreply.github.com>
2019-02-15 15:24:43 -0800

    use byte_range when reading CSVs (#78)

--- dask_cudf/io/csv.py
+++ dask_cudf/io/csv.py
@@ -1,22 +1,38 @@
+import os
 from glob import glob
 
 import cudf
 from dask.base import tokenize
 from dask.compatibility import apply
 import dask.dataframe as dd
+from dask.utils import parse_bytes
 
 
-def read_csv(path, **kwargs):
+def read_csv(path, chunksize="128 MiB", **kwargs):
+    if isinstance(chunksize, str):
+        chunksize = parse_bytes(chunksize)
     filenames = sorted(glob(str(path)))  # TODO: lots of complexity
     name = "read-csv-" + tokenize(path, **kwargs)  # TODO: get last modified time
 
     meta = cudf.read_csv(filenames[0], **kwargs)
 
-    graph = {
-        (name, i): (apply, cudf.read_csv, [fn], kwargs)
-        for i, fn in enumerate(filenames)
-    }
+    dsk = {}
+    i = 0
+    name = f"read-csv-{tokenize(path, chunksize, **kwargs)}"
+    for fn in filenames:
+        size = os.path.getsize(fn)
+        for start in range(0, size, chunksize):
+            kwargs2 = kwargs.copy()
+            kwargs2["byte_range"] = (
+                start,
+                chunksize,
+            )  # specify which chunk of the file we care about
+            kwargs2["dtype"] = meta.dtypes.values
+            if start != 0:
+                kwargs2["names"] = meta.columns  # no header in the middle of the file
+                kwargs2["header"] = None
+            dsk[(name, i)] = (apply, cudf.read_csv, [fn], kwargs2)
+            i += 1
 
-    divisions = [None] * (len(filenames) + 1)
-
-    return dd.core.new_dd_object(graph, name, meta, divisions)
+    divisions = [None] * (len(dsk) + 1)
+    return dd.core.new_dd_object(dsk, name, meta, divisions)
--- dask_cudf/io/tests/test_csv.py
+++ dask_cudf/io/tests/test_csv.py
@@ -1,6 +1,8 @@
 import dask
 import dask_cudf
 import dask.dataframe as dd
+import pandas as pd
+import numpy as np
 
 
 def test_read_csv(tmp_path):
@@ -11,3 +13,18 @@ def test_read_csv(tmp_path):
 
     df2 = dask_cudf.read_csv(tmp_path / "*.csv")
     dd.assert_eq(df, df2)
+
+
+def test_read_csv_w_bytes(tmp_path):
+    df = dask.datasets.timeseries(dtypes={"x": int, "y": int}, freq="120s").reset_index(
+        drop=True
+    )
+    df = pd.DataFrame(dict(x=np.arange(20), y=np.arange(20)))
+    df = dd.from_pandas(df, npartitions=1)
+    df.to_csv(tmp_path / "data-*.csv", index=False)
+
+    df2 = dask_cudf.read_csv(tmp_path / "*.csv", chunksize="50 B")
+
+    result = df2.compute().to_pandas()
+    expected = df.compute()
+    dd.assert_eq(result, expected, check_index=False)
